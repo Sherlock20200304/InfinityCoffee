3a52379c783718cf5d889fc055911f169a718a1f
Jessica <Jessica@192.168.0.11>
2020-07-14 00:07:02 -0400

    Adding Springboot

---
+++ InfinityCoffee/src/main/java/space/earth/InfinityCoffeeApplication.java
@@ -0,0 +1,13 @@
+package space.earth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityCoffeeApplication.class, args);
+	}
+
+}
